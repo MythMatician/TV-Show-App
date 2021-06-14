acad2ac2c31b450a886014ed2717507a36ef90ff
Kevin van Niekerk <37071525+MythMatician@users.noreply.github.com>
2021-06-14 06:28:04 +0200

    Add files via upload
    Initial commit

---
+++ app.js
@@ -0,0 +1,31 @@
+const form = document.querySelector('#searchForm');
+const div = document.querySelector('.show-list');
+const item = document.createElement('DIV');
+
+const makeData = (shows) => {
+    for(let results of shows) {
+        item.innerHTML = `
+        <img src='${results.show.image.medium}'></img>
+        <p><strong>Name: </strong>${results.show.name}</p>
+        <p><strong>Rating: </strong>${results.show.rating.average}</p>
+        <p><strong>Genres: </strong>${results.show.genres}
+        `;
+        
+    }
+    item.classList.add('style');
+    div.append(item);
+}
+
+form.addEventListener('submit', async (e) => {
+    e.preventDefault();
+    let search = form.elements.query.value;
+    const config = {params: {q: search}}
+    const res =  await axios.get(`http://api.tvmaze.com/search/shows`, config);
+    // const item = res.data;
+    // for(let result of item) {
+    //     console.log(result.show.id);
+    // }
+    // makeImages(res.data);
+    makeData(res.data);
+    console.log(res.data);
+});
