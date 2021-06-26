c6bdb972366f400637a4ac209db5d3009e5a3c8f
Kevin van Niekerk <37071525+MythMatician@users.noreply.github.com>
2021-06-26 10:59:30 +0200

    Update app.js

--- app.js
+++ app.js
@@ -1,31 +1,32 @@
 const form = document.querySelector('#searchForm');
+const input = document.querySelector('input');
 const div = document.querySelector('.show-list');
-const item = document.createElement('DIV');
 
 const makeData = (shows) => {
     for(let results of shows) {
-        item.innerHTML = `
-        <img src='${results.show.image.medium}'></img>
-        <p><strong>Name: </strong>${results.show.name}</p>
-        <p><strong>Rating: </strong>${results.show.rating.average}</p>
-        <p><strong>Genres: </strong>${results.show.genres}
-        `;
-        
+        if(results.show.image) {
+            const item = document.createElement('DIV');
+            item.innerHTML = `
+            <img src=${results.show.image.medium}>
+            <p><strong>Name: </strong>${results.show.name}</p>
+            <p><strong>Rating: </strong>${results.show.rating.average}</p>
+            <p><strong>Genres: </strong>${results.show.genres}
+            `;
+            item.classList.add('style');
+            div.append(item);
+        }
     }
-    item.classList.add('style');
-    div.append(item);
 }
 
 form.addEventListener('submit', async (e) => {
     e.preventDefault();
     let search = form.elements.query.value;
     const config = {params: {q: search}}
-    const res =  await axios.get(`https://api.tvmaze.com/search/shows`, config);
-    // const item = res.data;
-    // for(let result of item) {
-    //     console.log(result.show.id);
-    // }
-    // makeImages(res.data);
+    const res =  await axios.get(`http://api.tvmaze.com/search/shows`, config);
     makeData(res.data);
     console.log(res.data);
+});
+
+input.addEventListener('change', () => {
+    div.innerHTML = '';
 });
